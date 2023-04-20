155668c091eb85494e2297113692695d8baee456
mamuka111 <123620275+mamuka111@users.noreply.github.com>
2023-04-20 16:00:20 +0400

    Deployed website

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import FixedHeader from "./genericComponents/Header/FixedHeader/FixedHeader";
 // import FixedHeaderTemplate from "./modules/Header/FixedHeader/FixedHeaderTemplate";
-
+import Login from "./genericComponents/Header/staticHeader/StaticHeader"
 // css
 import "./css/reset.css";
 
 function App() {
   return (
     <>
+    <Login/>
       <FixedHeader />
     </>
   );
---
+++ src/genericComponents/Header/staticHeader/components/SignedinForm.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './SignedinForm.css';
+
+export default function SignedinForm() {
+  return (
+    <div className='SignedinForms'>
+    <h1>MY BOOKING</h1>
+    <h1>MY PROFILE&nbsp;</h1>
+    <h1>LOGOUT &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</h1>
+    </div>
+  )
+}
+
