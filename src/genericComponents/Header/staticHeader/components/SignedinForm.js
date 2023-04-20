9ebb24f2f3a834f94c6623609ba5547c1d12e3e5
mamuka111 <123620275+mamuka111@users.noreply.github.com>
2023-04-20 16:39:25 +0400

    Deployed website

--- src/genericComponents/Header/staticHeader/components/SignedinForm.js
+++ src/genericComponents/Header/staticHeader/components/SignedinForm.js
@@ -4,9 +4,9 @@ import './SignedinForm.css';
 export default function SignedinForm() {
   return (
     <div className='SignedinForms'>
-    <h1>MY BOOKING</h1>
-    <h1>MY PROFILE&nbsp;</h1>
-    <h1>LOGOUT &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</h1>
+      <h1 className='MYBOOKING'>MY BOOKING</h1>
+    <h1 className='MYPROFILE'>MY PROFILE&nbsp;</h1>
+    <h1 className='LOGOUT'>LOGOUT &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</h1>
     </div>
   )
 }
