00c4fa6611ca661a1e1755fd5f928b0ef6a2a426
mamuka111 <123620275+mamuka111@users.noreply.github.com>
2023-04-21 13:04:59 +0400

    Deployed website

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import FixedHeader from "./genericComponents/Header/FixedHeader/FixedHeader";
 // import FixedHeaderTemplate from "./modules/Header/FixedHeader/FixedHeaderTemplat
 import StaticHeader from "./genericComponents/Header/staticHeader/StaticHeader"
 // css
+import FooterTemplate from "./genericComponents/footer/components/FooterTemplate";
+import Footer from "./genericComponents/footer/components/Footer";
 import "./css/reset.css";
 
 function App() {
@@ -9,6 +11,7 @@ function App() {
     <>
     <StaticHeader/>
       <FixedHeader />
+      <Footer/>
     </>
   );
 }
---
+++ src/genericComponents/footer/components/Footer.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import FooterTemplate from './FooterTemplate'
+
+export default function Footer() {
+  return (
+    <>
+    <FooterTemplate
+    footerLogo={<g></g>}
+    footerLink={<g></g>}
+    fooetrServices={<g></g>}
+    footerContact={<g></g>}/>
+    </>
+  )
+}
