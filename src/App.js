b8135accfd3e337ad254e482d7d90c9baa13aaf3
mamuka111 <123620275+mamuka111@users.noreply.github.com>
2023-04-21 13:15:03 +0400

    Deployed website

--- src/App.js
+++ src/App.js
@@ -1,15 +1,11 @@
-import FixedHeader from "./genericComponents/Header/FixedHeader/FixedHeader";
 // import FixedHeaderTemplate from "./modules/Header/FixedHeader/FixedHeaderTemplat
-import StaticHeader from "./genericComponents/Header/staticHeader/StaticHeader"
 // css
-import FooterTemplate from "./genericComponents/footer/components/FooterTemplate";
-import Footer from "./genericComponents/footer/components/Footer";
 import "./css/reset.css";
 
 function App() {
   return (
     <>
-    
+
     </>
   );
 }
