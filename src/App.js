0735062737f87ac77b72ac92419a12c3ed94b438
gogolashvili <ggogolashvili91@gmail.com>
2023-04-21 14:36:46 +0400

    Deployed website

--- src/App.js
+++ src/App.js
@@ -2,14 +2,10 @@
 
 // css
 import "./css/reset.css";
+
 import FooterLogo from "./genericComponents/Footer/components/FooterLogo";
-
 function App() {
-  return (
-    <>
-
-    </>
-  );
+  return <></>;
 }
 
 export default App;
