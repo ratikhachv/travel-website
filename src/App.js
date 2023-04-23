bfe6ad06c4bcc6d890a9a1685af6ac48af107569
gogolashvili <ggogolashvili91@gmail.com>
2023-04-23 17:04:22 +0400

    add-home-page-template

--- src/App.js
+++ src/App.js
@@ -6,11 +6,13 @@ import Header from "./genericComponents/Header/Header";
 // css
 import "./css/reset.css";
 import "./css/General.css";
+import HomePage from "./pages/HomePage";
 
 function App() {
   return (
     <>
       <Header />
+      <HomePage />
       <Footer />
     </>
   );
