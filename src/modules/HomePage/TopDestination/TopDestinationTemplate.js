ba6217a6952abbd72d0c8d6413b6169f2f5952e3
mamuka111 <123620275+mamuka111@users.noreply.github.com>
2023-04-24 17:16:20 +0400

    Deployed website

--- src/modules/HomePage/TopDestination/TopDestinationTemplate.js
+++ src/modules/HomePage/TopDestination/TopDestinationTemplate.js
@@ -2,6 +2,12 @@ import React from 'react'
 import './TopDestinationTemplate.css'
 export default function TopDestinationTemplate(props) {
   return (
+    <>
+    <div id='mainDiv'>
+    <div id='commonDiv'>
+    <p className='TopDestination'>top destination</p>
+    <p className='DiscoverYourLove'>Discover your love</p>
+    </div>
     <div className='TopDestinationTemplateSection'>
         {props.boxes}
         {props.boxes}
@@ -10,5 +16,7 @@ export default function TopDestinationTemplate(props) {
         {props.boxes}
         {props.boxes}
     </div>
+    </div>
+    </>
   )
 }
