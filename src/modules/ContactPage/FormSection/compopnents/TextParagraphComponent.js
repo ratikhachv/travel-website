ba2b2bfb5c8d87b997943ef89829af712a46e166
mamuka111 <123620275+mamuka111@users.noreply.github.com>
2023-05-01 14:08:14 +0400

    done form

--- src/modules/ContactPage/FormSection/compopnents/TextParagraphComponent.js
+++ src/modules/ContactPage/FormSection/compopnents/TextParagraphComponent.js
@@ -6,6 +6,7 @@ export default function TextParagraphComponent() {
     <div className='headingParagraphDiv'>
         <p className='headingP-tag'>Get in touch</p>
         <p className='textLorem'>Lorem ipsum dolor sit amet, consectetur adipiscing elit ut aliquam, purus sit amet luctus venenatis, lectus magna fringilla urna, porttitor rhoncus dolor purus non enim praesent elementum facilisis leo, vel fringilla est ullamcorper eget nulla facilisi etiam dignissim diam quis enim lobortis scelerisque fermentum dui faucibus in ornare quam viverra</p>       
+    
     </div>
         </>
   )
