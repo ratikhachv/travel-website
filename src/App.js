a402c43dd0e1ec0f9dd7ee29d8d43516252dccee
gogolashvili <ggogolashvili91@gmail.com>
2023-05-09 15:58:38 +0400

    add info text component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
-import Footer from "./genericComponents/Footer/Footer";
-import Header from "./genericComponents/Header/Header";
+// import Footer from "./genericComponents/Footer/Footer";
+// import Header from "./genericComponents/Header/Header";
 // import HomePage from "./pages/HomePage";
 // import NewsPage from "./pages/NewsPage";
-import ContactPage from "./pages/ContactPage";
+// import ContactPage from "./pages/ContactPage";
 // css
 import "./css/reset.css";
 import "./css/General.css";
