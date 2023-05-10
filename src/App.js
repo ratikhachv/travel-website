339c62eeb7405a57d0360dab6d8d14b9883ae4d5
Lasha <lashagoglidzelasha@gmail.com>
2023-05-10 16:34:16 +0400

    lasha files

--- src/App.js
+++ src/App.js
@@ -1,20 +1,22 @@
-// import Footer from "./genericComponents/Footer/Footer";
-// import Header from "./genericComponents/Header/Header";
-// import HomePage from "./pages/HomePage";
-// import NewsPage from "./pages/NewsPage";
-// import ContactPage from "./pages/ContactPage";
+import Footer from "./genericComponents/Footer/Footer";
+import Header from "./genericComponents/Header/Header";
+import HomePage from "./pages/HomePage";
+import NewsPage from "./pages/NewsPage";
+import ContactPage from "./pages/ContactPage";
 // css
 import "./css/reset.css";
 import "./css/General.css";
+import OurTeamTemplate from "./modules/AboutUsPage/OurTeam/OurTeamTemplate";
 
 function App() {
 
   return (
     <>
-      {/* <Header /> */}
-      {/* <ContactPage /> */}
-      {/* <NewsPage /> */}
-      {/* <Footer /> */}
+      {/* <Header />
+      <ContactPage />
+      <NewsPage />
+      <Footer /> */}
+      <OurTeamTemplate/>
     </>
   );
 
